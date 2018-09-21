639e43fc929bab8abc48ba792b351674a00d8369
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-21 10:13:12 +0200

    update

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "remote_state_bucket_region" {
   default     = "eu-west-1"
 }
 variable "remote_state_bucket" {
-  default = "tf-state-bodyxsddzhllgkrg"
+  default = "tf-state-ayhynmmqenhbbtjy"
 }
 variable "remote_state_key" {
   default = "baseinfrastruktur.state"
